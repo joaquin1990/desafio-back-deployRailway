7a52f19a649bf17369bfeb146a85ff1b5fd2d6df
joaquin1990 <joaquingarese@gmail.com>
2022-10-24 11:01:05 -0300

    Adding PORT to variables

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.listen(8080, () => console.log("App listening on 8080"));
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 app.get("/", (req, res) => {
   res.send("Probando Depoly de Railway");
