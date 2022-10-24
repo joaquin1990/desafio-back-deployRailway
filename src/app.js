db1643a2eb29a8596c8893333c937f2fe8542c05
joaquin1990 <joaquingarese@gmail.com>
2022-10-24 10:55:11 -0300

    Adding start script in Json

--- src/app.js
+++ src/app.js
@@ -5,5 +5,9 @@ const app = express();
 app.listen(8080, () => console.log("App listening on 8080"));
 
 app.get("/", (req, res) => {
-  res.send({ status: "sucess", message: "listo" });
+  res.send("Probando Depoly de Railway");
+});
+
+app.get("/principal", (req, res) => {
+  res.send("<h1>Probando deploy de Railway</h1>");
 });
